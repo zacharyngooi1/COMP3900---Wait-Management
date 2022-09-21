7ff83d7062b447de9a26f60464cd21b5b7d8b9b2
zacha <zacharyngooi@hotmail.com>
2022-09-21 15:23:06 +1000

    Tested database connection

---
+++ testsql.py
@@ -0,0 +1,22 @@
+import mysql.connector
+from mysql.connector import Error
+import pandas as pd
+
+
+def create_server_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
+
+pw = "Comp390012!"
+
+connection = create_server_connection("localhost", "root", pw)
