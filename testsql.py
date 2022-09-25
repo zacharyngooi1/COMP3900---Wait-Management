676a30117bac58a9a9c1e9fbaf129f72967e8d09
zacha <zacharyngooi@hotmail.com>
2022-09-26 07:41:18 +1000

    Some test functions for table creation

--- testsql.py
+++ testsql.py
@@ -3,13 +3,14 @@ from mysql.connector import Error
 import pandas as pd
 
 
-def create_server_connection(host_name, user_name, user_password):
+def create_server_connection(host_name, user_name, user_password,database):
     connection = None
     try:
         connection = mysql.connector.connect(
             host=host_name,
             user=user_name,
-            passwd=user_password
+            passwd=user_password,
+            database = database
         )
         print("MySQL Database connection successful")
     except Error as err:
@@ -19,4 +20,27 @@ def create_server_connection(host_name, user_name, user_password):
 
 pw = "Comp390012!"
 
-connection = create_server_connection("localhost", "root", pw)
+mydb = create_server_connection("localhost", "root", pw, "wait_management")
+
+mycursor = mydb.cursor()
+
+def show_databases():
+    mycursor.execute("SHOW DATABASES")
+    for x in mycursor:
+      print(x)
+
+
+def create_restaurant(restaurant_name):
+
+    mycursor.execute("CREATE TABLE %s (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))" % restaurant_name)
+
+def show_tables():
+
+    mycursor.execute("SHOW TABLES")
+    for x in mycursor:
+        print(x)
+
+print("databases are:")
+show_databases()
+print("tables are:")
+show_tables()
